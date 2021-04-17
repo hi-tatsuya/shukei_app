fa952fb8cdb62618446bde1e138a9c39dec6a18e
hiyamatatsuya <tatsuyashonan@gmail.com>
2021-04-17 11:39:16 +0900

    自分の見込みページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root to: 'mikomis#index'
   resources :mikomis
   devise_for :users
+  resource :users, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
